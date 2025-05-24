d59a134aa2adfc23dea8133d35fc15293afeb7de
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-24 19:24:13 +0100

    further work to be done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use scraper;
+use reqwest::blocking::get;
+
 fn main() {
-    println!("Hello, world!");
+    let test_link = get("https://tau.edu.ng")?.text()?;
+
+    let document = scraper::Html::parse_document(test_link);
+    let h3_selector = scraper::Selector::parse("h4").unwrap();
+
+    if let Some(h3_element) = document.select(&h3_selector).next(){
+        let h3_text = h3_element.text().collect::<String>();
+        println!("{:?}", h3_text);
+    } else{
+        println!("No tag found");
+    }
 }
